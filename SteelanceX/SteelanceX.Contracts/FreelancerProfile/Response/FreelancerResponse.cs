acd8116116d9d04564fb09e3da640a99986535c6
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-06-09 20:48:10 +0700

    fix

--- SteelanceX/SteelanceX.Contracts/FreelancerProfile/Response/FreelancerResponse.cs
+++ SteelanceX/SteelanceX.Contracts/FreelancerProfile/Response/FreelancerResponse.cs
@@ -10,11 +10,8 @@ public class FreelancerResponse
 {
     public int Id { get; set; }
     public string Description { get; set; }
-    public string ResumeUrl { get; set; }
 
     public int AppUserId { get; set; }
-    public virtual AppUser AppUser { get; set; }
-    public virtual ICollection<Application> Applications { get; set; }
-    public virtual ICollection<JobProgress> JobProgress { get; set; }
-    public virtual ICollection<FreelancerCategory> Categories { get; set; }
+    public string Fullname { get; set; }
+    public List<int> Categories { get; set; }
 }
--- SteelanceX/SteelanceX.Domain/Models/FreelancerProfile.cs
+++ SteelanceX/SteelanceX.Domain/Models/FreelancerProfile.cs
@@ -4,8 +4,11 @@ public class FreelancerProfile
 {
     public int Id { get; set; }
     public string Description { get; set; }
+    public string ResumeUrl { get; set; }
 
     public int AppUserId { get; set; }
-    public string Fullname  { get; set; }
-    public List<int> Categories { get; set; }
+    public virtual AppUser AppUser { get; set; }
+    public virtual ICollection<Application> Applications { get; set; }
+    public virtual ICollection<JobProgress> JobProgress { get; set; }
+    public virtual ICollection<FreelancerCategory> Categories { get; set; }
 }
