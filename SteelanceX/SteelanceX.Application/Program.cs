8e8f6e1313cddd4ea2cd824a078b1300a7a8068f
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-06-18 11:05:19 +0700

    Update Program.cs

--- SteelanceX/SteelanceX.Application/Program.cs
+++ SteelanceX/SteelanceX.Application/Program.cs
@@ -137,6 +137,7 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
     builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
